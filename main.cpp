ab5f8d4e7ea1a58d0f8dab1c3bc16f57e3738cd3
Roy Nevo <theroyn@gmail.com>
2026-08-23 01:38:42 +0300

    static init of vars

--- main.cpp
+++ main.cpp
@@ -1,33 +1,22 @@
 #include <cstdint>
 
-volatile int hihi;
-struct TestInitArray
-{
-    TestInitArray()
-    {
-        hihi = 0xc0ffee;
-    }
-};
 
-TestInitArray test;
+constexpr uintptr_t iRCC = 0x40023800;
+constexpr uintptr_t iAHB1ENR = iRCC + 0x30;
+constexpr uintptr_t iGPIOC = 0x40020800;
+constexpr uintptr_t iModer = iGPIOC + 0x00;
+constexpr uintptr_t iODR = iGPIOC + 0x14;
 
-int* iRCC = (int*)0x40023800;
- // must cast to uintptr_t so interger arithmetic is used rather than pointer arithmetic
- // which silently multiplies the added offsets by sizeof(int)
-volatile int* iAHB1ENR = (int*)((uintptr_t)iRCC + 0x30);
-int* iGPIOC = (int*)0x40020800;
-volatile int* iModer = (int*)((uintptr_t)iGPIOC + 0x00);
-volatile int* iODR = (int*)((uintptr_t)iGPIOC + 0x14);
 int main()
 {
-    *iAHB1ENR |= 0b100;
-    *iModer = (*iModer & ~0xc000000) | 0x4000000; // zero-out the 2-bit mask and set bit 26
+    *(volatile uint32_t*)iAHB1ENR |= 0b100;
+    *(volatile uint32_t*)iModer = (*(volatile uint32_t*)iModer & ~0xc000000) | 0x4000000; // zero-out the 2-bit mask and set bit 26
     for(unsigned int cnt=0;;++cnt)
     {
         if((cnt/500000) % 2 == 0)
-            *iODR |= 0x2000;
+            *(volatile uint32_t*)iODR |= 0x2000;
         else
-            *iODR &= ~0x2000;
+            *(volatile uint32_t*)iODR &= ~0x2000;
     }
 
     return 0;
