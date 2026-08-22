c7f47b2e895653e51904b564c9659c4435dfa700
Roy Nevo <theroyn@gmail.com>
2026-08-22 23:58:52 +0300

    blinky working!

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-volatile int hihi = 0;
+#include <cstdint>
+
+volatile int hihi;
 struct TestInitArray
 {
     TestInitArray()
@@ -9,7 +11,24 @@ struct TestInitArray
 
 TestInitArray test;
 
+int* iRCC = (int*)0x40023800;
+ // must cast to uintptr_t so interger arithmetic is used rather than pointer arithmetic
+ // which silently multiplies the added offsets by sizeof(int)
+volatile int* iAHB1ENR = (int*)((uintptr_t)iRCC + 0x30);
+int* iGPIOC = (int*)0x40020800;
+volatile int* iModer = (int*)((uintptr_t)iGPIOC + 0x00);
+volatile int* iODR = (int*)((uintptr_t)iGPIOC + 0x14);
 int main()
 {
+    *iAHB1ENR |= 0b100;
+    *iModer |= 0x4000000;
+    for(int cnt=0;;++cnt)
+    {
+        if((cnt/50000) % 2 == 0)
+            *iODR |= 0x2000;
+        else
+            *iODR &= ~0x2000;
+    }
+
     return 0;
 }
