978db131e973c81677636ffbad8e2536badc8379
Roy Nevo <theroyn@gmail.com>
2026-08-29 21:41:51 +0300

    moved blinky to another TU

--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
-#include <cstdint>
-
-
-constexpr uintptr_t iRCC = 0x40023800;
-constexpr uintptr_t iAHB1ENR = iRCC + 0x30;
-constexpr uintptr_t iGPIOC = 0x40020800;
-constexpr uintptr_t iModer = iGPIOC + 0x00;
-constexpr uintptr_t iODR = iGPIOC + 0x14;
+#include "blinky.h"
 
 int main()
 {
-    *(volatile uint32_t*)iAHB1ENR |= 0b100;
-    *(volatile uint32_t*)iModer = (*(volatile uint32_t*)iModer & ~0xc000000) | 0x4000000; // zero-out the 2-bit mask and set bit 26
-    for(unsigned int cnt=0;;++cnt)
-    {
-        if((cnt/500000) % 2 == 0)
-            *(volatile uint32_t*)iODR |= 0x2000;
-        else
-            *(volatile uint32_t*)iODR &= ~0x2000;
-    }
-
+    run_blinky();
+    
     return 0;
 }
